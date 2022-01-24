c40a0c78d10858e45be9df15ddf79f1db7746d35
Pablo Lopera <drlopera@gmail.com>
2022-01-24 17:11:52 -0500

    Add files via upload

---
+++ PROG8400-22W - Assigment 01 - Part1.sql
@@ -0,0 +1,57 @@
+-- Team
+-- Group Members are:
+--    MAKINDE, Olureime
+--    LOPERA, Pablo
+-- test
+--
+--
+--1. List all data from the AcademicStatus table. AcademicStatus 1
+--
+select *
+from   AcademicStatus
+
+--2. List number and academicStatusCode for all students who  have been discontinued. List the results in descending order
+--by number.
+--Student 1
+--
+
+
+--3. List number and academicStatusCode for all students who  have been discontinued or suspended. List the results in
+--ascending order by number. Use OR. Do not use NOT or !=.
+--Student 1
+
+
+--4. List number and academicStatusCode for all students who have been discontinued or suspended. List the results in
+--ascending order by number. Use NOT. Do not use OR or !=.
+--Student 1
+
+
+--5. List number and academicStatusCode for all students who have been discontinued or suspended. List the results in
+--ascending order by number. Use !=. Do not use OR or NOT.
+--Student 1
+
+--6. List the countries persons are from, but show each country code only once.
+--Person 1
+
+--7. College management would like to see the new values that will result if all incidental fees are increased by 10%. Show
+--item aliased as ‘Incidental Fee Item’. Show amountPerSemester aliased as ‘Current Fee’. Use an
+--expression to calculate the increased fee and alias the output as ‘Increased Fee’. Order the results in ascending
+--order by item.
+--The values created by your expression will have many decimal places. Later we will discuss how to format money
+--amounts in a more conventional form.
+--IncidentalFee 1
+
+
+--8. List all data for students with final mark less than (but not equal to) 55 and greater than 0.
+--CourseStudent 1
+
+--9. List the number of any room that has a capacity greater than or equal to 40, is a lab, has computers with 4GB
+--memory, and is located at the Doon campus.
+--Room 1
+
+
+--10. List all employees who teach in the School of Trades and Apprenticeship and are located at the Doon, Guelph or
+--Waterloo campus.
+--Employee 1
+
+
---
+++ PROG8400-22W - Assigment 01 - Part2.sql
@@ -0,0 +1,66 @@
+-- Team
+-- Group Members are:
+--    MAKINDE, Olureime
+--    LOPERA, Pablo
+
+--
+--
+--1. DATABASE AND TABLE(s) CREATION:
+-- Create the “Messy” database manually (by using SSMS menus)
+-- Using the script provided, create the “Messy” database table within the newly created
+-- “Messy” database
+
+-- Done.   Database and table created without problems
+
+
+
+--
+--
+--2. AFTER DONE WITH THE TABLE CREATION, OBSERVE THE DATABASE STRUCTURE AND TADA
+--AND COMMENT ON THE FOLLOWING:
+--
+--2.1. The “Messy” table for errors, inconsistencies in its structure, any/every kind, data duplication,
+--missing/omitted data, etc. For example, you may notice that a number of relations contain
+--more that one value, which violates relational database design principles.
+--
+--
+
+--
+--
+--3. ANALYSIS OF THE DATABASE BY COMPOSING SQL STATEMENTS:
+--
+-- 3.1 Compose (write) and run appropriate SQL statements to analyze the “Messy” table for any
+--inconsistencies you have identified in the requirement 2. For example, you may want to find
+--out the number of actual customers in the Messy table, that contains over 24,000 records.
+--Are there 24,000 customers or actually less than that. You may also try to analyze and
+--document on how and why you think this happened that the number of customers is
+--substantially less than the overall number of records within the Messy table.
+--
+--
+
+
+--
+--
+--4. TASKS REQUIRING ONLINE RESEARCH:
+--
+-- 4.1. Conduct online research in order to create the necessary SQL queries that will assist you with
+--the analysis. For example, you may want to analyze the amount of missing/omitted data
+--within the Messy table. In order to do this, you might need to “count” some of the missing
+--data. Your creativity is highly encouraged.
+-- 4.2 Please document the online sources you have used as commented out text within your SQL
+--script to be submitted.
+--
+--
+
+
+
+--
+--
+-- 5. BASED ON THE RESEARCH CONDUCTED, COMPOSE THE BUSINESS RULES, WHICH YOU THINK
+-- WERE LIKELY USED BY THE OLD BUSINESS, WHILE DESIGNING AND STORING DATA IN ITS
+-- Messy TABLE.
+--
+--
+
+
+
